b0b1fbae5f76e082409b91673fd7bc07e299b91a
Elliza Megawati Ritonga <38091851+elliza123@users.noreply.github.com>
2018-07-31 16:24:27 +0700

    index.php

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['id_admin'])) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Job Portal</title>
+  <title>Masuk Sebagai Admin Cari Kerja</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -40,11 +40,11 @@ if(isset($_SESSION['id_admin'])) {
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../index.php"><b>Job</b> Portal</a>
+    <a href="../index.php"><b>Cari</b> Kerja</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Admin Login</p>
+    <p class="login-box-msg">Masuk Sebagai Admin</p>
 
     <form action="checklogin.php" method="post">
       <div class="form-group has-feedback">
@@ -58,7 +58,7 @@ if(isset($_SESSION['id_admin'])) {
       <div class="row">
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
         </div>
         <!-- /.col -->
       </div>
