b6f92412816b1e62e4fc6ca48cdb7779bb569bc8
Elliza Megawati Ritonga <38091851+elliza123@users.noreply.github.com>
2018-07-31 15:23:43 +0700

    update login-candidates.php

--- login-candidates.php
+++ login-candidates.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Job Portal</title>
+  <title>Masuk Sebagai Pencari Kerja</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -38,11 +38,11 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index.php"><b>Job</b> Portal</a>
+    <a href="index.php"><b>Cari</b> Kerja</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Candidates Login</p>
+    <p class="login-box-msg">Masuk Sebagai Pekerja</p>
 
     <form method="post" action="checklogin.php">
       <div class="form-group has-feedback">
@@ -55,11 +55,11 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
       </div>
       <div class="row">
         <div class="col-xs-8">
-          <a href="#">I forgot my password</a>
+          <a href="#">Lupa Password?</a>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
         </div>
         <!-- /.col -->
       </div>
